fb3905a51b9620f62cdd360d7f58b73c32de6c06
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2024-12-05 00:32:24 +0900

    a

--- src/ts/pageLinks.ts
+++ src/ts/pageLinks.ts
@@ -1,5 +1,6 @@
 export const LINKS = {
   home: { href: '/', text: 'home', title: 'HOME' },
   works: { href: '/works', text: 'work', title: 'WORKS' },
+  create: { href: '/create', text: 'create', title: 'CREATE' },
   links: { href: '/links', text: 'person', title: 'LINKS' },
 } as const;
